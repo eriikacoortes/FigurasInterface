40cfa384ac24559734e4957296271febfc180d70
usuario <julianchom8@gmail.com>
2020-03-04 22:46:47 -0500

    Se agrega clase abstracta Se crea Interface con los metodos figuras se implementan metodos de la interface Se crea boton para guardar cordenadas en un array

---
+++ src/figuras/mejoradas/ImetodosFiguras.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package figuras.mejoradas;
+
+/**
+ *
+ * @author usuario
+ */
+public interface ImetodosFiguras {
+     public void calcularArea();
+     public void calcularPerimetro();
+     public boolean validarPuntos();
+    
+}
---
+++ src/figuras/mejoradas/ListFiguras.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package figuras.mejoradas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author usuario
+ */
+public class ListFiguras {
+    List<Puntos> lista = new ArrayList();
+    Puntos v= lista.get(1);
+    
+    
+    //Puntos p = (Puntos)p;
+    
+}
